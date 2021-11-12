2afca2f97e1d3c6155cc8024cf69519caf5203ee
KronosGR <geo.elgeo@gmail.com>
2021-11-12 07:12:31 +0100

    Return a more detailed error

--- app.js
+++ app.js
@@ -49,10 +49,18 @@ app.use(
   graphqlHTTP({
     schema: graphqlSchema,
     rootValue: graphqlResolver,
-    graphiql: true
+    graphiql: true,
+    formatError(err) {
+      if (!err.originalError) {
+        return err;
+      }
+      const data = err.originalError.data;
+      const message = err.message || 'An error occured';
+      const code = err.originalError.code || 500;
+      return { message: message, status: code, data: data };
+    },
   })
 );
-
 
 app.use((error, req, res, next) => {
   console.log(error);
