eb0f9599ab075399ee19f6958058bd8d5255a784
KronosGR <geo.elgeo@gmail.com>
2021-11-13 21:09:50 +0100

    Fixed createPost save

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -108,6 +108,7 @@ module.exports = {
 
     const createdPost = await post.save();
     user.posts.push(createdPost)
+    await user.save();
     return {
       ...createPost._doc,
       _id: createdPost._id.toString(),
